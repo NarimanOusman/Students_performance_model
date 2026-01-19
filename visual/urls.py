f941c72798bc438f1f1665ecc4a945b08a28644b
NarimanOusman <narimanrobo8@gmail.com>
2026-01-19 11:11:12 -0800

    project setup

---
+++ visual/urls.py
@@ -0,0 +1,8 @@
+# visuals/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='Home'),
+    path('dashboard/', views.dashboard, name='Dashboard'),
+]
